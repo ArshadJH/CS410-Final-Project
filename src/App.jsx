aad89bd7f79e07d41ef2c9708452cfd48172f90f
Arshad Hussain <43156121+ArshadJH@users.noreply.github.com>
2023-11-25 15:22:26 -0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CodeSubmission from './CodeSubmission';
+
+export function App(props) {
+  return (
+    <div>
+      <CodeSubmission />
+    </div>
+  );
+}
+
+// Log to console
+console.log('Hello console')
